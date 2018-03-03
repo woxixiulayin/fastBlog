63ac563255a4c74c6322bd17af78e1de2888ebc9
Jackson Liu <416883813@qq.com>
2018-03-03 13:45:03 +0800

    add style, basic react

--- public/src/index.js
+++ public/src/index.js
@@ -1,1 +1,13 @@
-console.log(2)
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './routes'
+
+import 'css/index.scss'
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#app')
+)
+
+
+
