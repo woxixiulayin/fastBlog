6c066544d34f4ba00ab9a04ef702ca46f8a2bb84
Jackson Liu <416883813@qq.com>
2018-03-04 14:28:03 +0800

    add router aconfig

--- public/src/routes/index.jsx
+++ public/src/routes/index.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
-import { Route, HashRouter} from 'react-router-dom'
-import App from 'src/views/app'
+import { Route, HashRouter, Redirect, Switch } from 'react-router-dom'
+import navConfig from './config'
+import App from 'src/views/App'
 
 const Home = () => <h1>this is home</h1>
+const navs = Object.values(navConfig)
 
 class Router extends Component {
     render() {
         return (
             <HashRouter>
                 <App>
-                    <Route path="/" component={Home} />
+                    {/* 保证只有一个路由被匹配到 */}
+                    <Switch>
+                        {
+                            navs.map(navItem => <Route
+                                exact
+                                key={navItem.label}
+                                path={navItem.path}
+                                component={navItem.component} />)
+                        }
+                        <Redirect to={navConfig.posts.path} />
+                    </Switch>
                 </App>
             </HashRouter>
         )
--- public/src/views/app/Header.jsx
+++ public/src/views/app/Header.jsx
@@ -1,5 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Header = () => <header>this is Header</header>
+class Header extends Component {
+    render() {
+        return (
+            <header
+             className="text-center"
+            >
+                <nav>
+                    <ul>
+
+                    </ul>
+                </nav>
+                <h2>Jackson Liu</h2>
+            </header>
+        )
+    }
+}
 
 export default Header
