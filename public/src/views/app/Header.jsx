4c480440ad7d2b9da1b0cc71223c3d8da8e63208
Jackson Liu <416883813@qq.com>
2018-03-04 15:15:59 +0800

    添加关于和github

--- public/src/views/app/Header.jsx
+++ public/src/views/app/Header.jsx
@@ -9,7 +9,7 @@ class Header extends Component {
              className="text-center relative"
             >
                 <nav className="nav clearfix">
-                    <div className="links left">
+                    <div className="links">
                     {navList.map(item => 
                         <NavLink
                           exact
@@ -22,6 +22,7 @@ class Header extends Component {
                             {item.zH}
                         </NavLink>
                     )}
+                    <a href="https://github.com/woxixiulayin" target="_blank">github</a>
                     </div>
                 </nav>
                 <h2>Jackson Liu</h2>
